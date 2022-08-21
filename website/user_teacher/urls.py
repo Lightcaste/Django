4502a667f68811bc56212d8b884c87520e6ff855
LightCaste <nguyenvantuan24689@gmail.com>
2022-08-21 07:59:42 +0700

    update 21.08

--- website/user_teacher/urls.py
+++ website/user_teacher/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.Teacher_main,name='main_screen'),
     path('change-information', views.Teacher_changing_information,name='teacher_change_information'),
     path('table-student',views.showStudent,name='show_student'),
-    path('create-question',views.showStudent,name='create-question'),
+    path('create-question',views.Create_Question,name='create-question'),
 
 ]
 
